f43647080a8dbef3a7d13a2cd6b6565a7be52927
Didar <didarkuliew54@gmail.com>
2025-04-15 11:29:42 +0500

    Password Reset Functionallity

--- users/urls.py
+++ users/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from users import views
 
 app_name = 'users' 
@@ -9,4 +10,23 @@ urlpatterns = [
     path('resend-code/', views.resend_code_view, name='resend-code'),
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
+
+    # Password reset URLs
+    path('forgot-password/', auth_views.PasswordResetView.as_view(
+    template_name='users/forgot_password.html',
+    email_template_name='users/password_reset_email.html',
+    subject_template_name='users/password_reset_subject.txt',
+    ), name='password_reset'),
+
+    path('forgot-password/done/', auth_views.PasswordResetDoneView.as_view(
+        template_name='users/password_reset_done.html'
+    ), name='password_reset_done'),
+
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
+    template_name='users/password_reset_confirm.html'
+    ), name='password_reset_confirm'),
+
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(
+        template_name='users/password_reset_complete.html'
+    ), name='password_reset_complete'),
 ]
